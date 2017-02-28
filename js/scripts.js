2ab23b294061785ff6a6abda4f1cd56757bf472b
Uneeck <urbantomasz87@gmail.com>
2017-02-28 08:43:00 +0100

    poprawka

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ var list = document.getElementById('list');
 var add = document.getElementById('addElem');
 var listLength = document.getElementsByTagName('li').length;
 add.addEventListener('click', function(){
-	listLength++;
+	
 	list.innerHTML+='<li>item ' + listLength + '</li>';
-
+	listLength++;
 });
